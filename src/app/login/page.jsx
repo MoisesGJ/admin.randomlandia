5265011e0bce3158be6d3b21a15090c4bdd47bf6
Moisés García <mgarciaj1802@alumno.ipn.mx>
2024-08-19 18:39:54 -0600

    fix: design

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -124,24 +124,42 @@ export default function Login() {
           />
         </div>
 
-        <h2>Autenticación sin contraseñas</h2>
+        <h2>Elige una opción:</h2>
 
         <button
           type="submit"
           name="action"
           value="magic-link"
-          className="border-[1.5px] p-2 border-blue hover:bg-blue hover:text-white hover:font-bold rounded-md w-full max-w-48"
+          className="border-[1.5px] p-2 border-blue hover:bg-blue hover:text-white hover:font-bold hover:fill-white rounded-md w-full max-w-48 flex gap-3 items-center justify-center"
           onClick={() => setAction("magic-link")}
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="18px"
+            viewBox="0 -960 960 960"
+            width="18px"
+            fill="fill-blue"
+          >
+            <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280 320-200v-80L480-520 160-720v80l320 200Z" />
+          </svg>
           Enviar correo
         </button>
         <button
           type="submit"
           name="action"
           value="biometric"
-          className="border-[1.5px] p-2 border-blue hover:bg-blue hover:text-white hover:font-bold rounded-md w-full max-w-48"
+          className="border-[1.5px] p-2 border-blue hover:bg-blue hover:text-white hover:fill-white hover:font-bold rounded-md w-full max-w-48 flex gap-3 items-center justify-center"
           onClick={() => setAction("biometric")}
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="18px"
+            viewBox="0 -960 960 960"
+            width="18px"
+            fill="fill-blue"
+          >
+            <path d="M280-240q-100 0-170-70T40-480q0-100 70-170t170-70q81 0 141.5 45.5T506-560h414v160h-80v160H680v-160H506q-24 69-84.5 114.5T280-240Zm0-160q33 0 56.5-23.5T360-480q0-33-23.5-56.5T280-560q-33 0-56.5 23.5T200-480q0 33 23.5 56.5T280-400Z" />
+          </svg>
           Biométricos
         </button>
       </form>
